de7ec6f44c611cd046832e7594b4d2b4d0f626ac
mercury-Hg80 <150161680+mercury-Hg80@users.noreply.github.com>
2025-02-20 15:54:59 -0500

    Remove: grid function

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const container = document.querySelector(".container");
             for (j = 0; j < 16; j++) {
                 const row = document.createElement("div");
                 container.appendChild(row);
-                
+
+        }
         }
     }
