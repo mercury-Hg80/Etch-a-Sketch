88910483eead3dc80b3e399ec087acc2b518b3b7
mercury-Hg80 <150161680+mercury-Hg80@users.noreply.github.com>
2025-02-20 15:51:43 -0500

    Add: grid function, container change to flex

---
+++ script.js
@@ -0,0 +1,10 @@
+const container = document.querySelector(".container");
+
+    function makeGrid() {
+        for (i = 0; i < 16; i++) {
+            for (j = 0; j < 16; j++) {
+                const row = document.createElement("div");
+                container.appendChild(row);
+                
+        }
+    }
