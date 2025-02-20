8246d3571bd0b443124906928472ae26771a0dde
mercury-Hg80 <150161680+mercury-Hg80@users.noreply.github.com>
2025-02-20 16:25:22 -0500

    Add: loop to create 16 divs append to container parent

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const container = document.querySelector(".container");
+const container = document.querySelector("container");
 
-    function makeGrid() {
-        for (i = 0; i < 16; i++) {
-            for (j = 0; j < 16; j++) {
-                const row = document.createElement("div");
-                container.appendChild(row);
 
-        }
-        }
+
+function makeDivs() {
+    for (i = 0; i < 16; i++) {
+        const newDiv = document.createElement('div');
+        div.classList.add()
+        container.appendChild(newDiv);
     }
+}
