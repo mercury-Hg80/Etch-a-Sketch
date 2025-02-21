688d4f1ea15b8413bf3449507a3b805c16da0486
mercury-Hg80 <150161680+mercury-Hg80@users.noreply.github.com>
2025-02-21 16:54:32 -0500

    Feat: mouseover squares now leaves classic etch-a-sketch trail

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 const container = document.querySelector(".container");
 
-
+function promptUser() {
+    let request = prompt("Please enter the amount of squares per side for the new grid would you like:", "");
+}
 
 function makeDivs() {
     for (i = 0; i < 16; i++) {
         const div = document.createElement('div');
-        div.classList.add('.square');
+        div.classList.add('square');
         container.appendChild(div);
+
+        div.addEventListener('mouseover', () => { 
+            div.style.backgroundColor = 'black';
+        })
+        /*
+        div.addEventListener('mouseout', () => {
+            div.style.backgroundColor = '#b2b6b2';
+
+        }) 
+        */
     }
+
 }
+
+makeDivs();
